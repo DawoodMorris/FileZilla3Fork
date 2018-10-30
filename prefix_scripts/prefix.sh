ebf8d6ca1699ab0d5824ce78a4c5cae701ad3193
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2018-10-30 10:10:33 +0000

    Allow plugging together multiple prefixes

--- prefix_scripts/prefix.sh
+++ prefix_scripts/prefix.sh
@@ -1,55 +1,95 @@
 #! /bin/sh
 
-export PREFIX="$1"
-
 if [ -z "$OLDPATH" ]; then
   export OLDPATH="$PATH"
 fi
 
-unset LD_LIBRARY_PATH
-unset CPPFLAGS
-unset LDFLAGS
-export PATH="$OLDPATH"
-unset PKG_CONFIG_PATH
-unset CONFIGURE_ARGS
-unset WINEARCH
-unset WINEPREFIX
-
-if [ -z "$PREFIX" ]; then
+clean()
+{
   unset PREFIX
-  echo "Prefix has been unset"
-  return 0
-else
+  unset PREFIXES
+  unset LD_LIBRARY_PATH
+  unset CPPFLAGS
+  unset LDFLAGS
+  export PATH="$OLDPATH"
+  unset PKG_CONFIG_PATH
+  unset CONFIGURE_ARGS
+  unset WINEARCH
+  unset WINEPREFIX
+}
+
+prepend()
+{
+  PREFIX="$1"
   if ! [ -d "$PREFIX" ]; then
     if [ -d "prefix-$PREFIX" ]; then
       PREFIX="prefix-$PREFIX"
     elif [ -d "$HOME/prefix-$PREFIX" ]; then
       PREFIX="$HOME/prefix-$PREFIX"
     else
-      echo "Invalid prefix"
-      unset PREFIX
+      echo "Invalid prefix $1"
       return 1
     fi
   fi
-  PREFIX=`realpath "$PREFIX"`
+
+  if which realpath > /dev/null 2>&1; then
+    export PREFIX=`realpath "$PREFIX"`
+  else
+    if pushd $PREFIX > /dev/null 2>&1; then
+      export PREFIX=`pwd`
+      popd > /dev/null || return 1
+    fi
+  fi
 
   if [ ! -d "$PREFIX" ]; then
-    echo "Invalid prefix"
-    unset PREFIX
+    echo "Invalid prefix $1"
     return 1
   fi
 
-  export PATH="$PREFIX/bin:$OLDPATH"
-  export LD_LIBRARY_PATH="$PREFIX/lib"
-  export CPPFLAGS="-I$PREFIX/include"
-  export LDFLAGS="-L$PREFIX/lib"
-  export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
+  export PREFIXES=$PREFIX${PREFIXES:+ $PREFIXES}
+
+  export PATH="$PREFIX/bin${PATH:+:$PATH}"
+  export LD_LIBRARY_PATH="$PREFIX/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+  export CPPFLAGS="-I$PREFIX/include${CPPFLAGS:+ $CPPFLAGS}"
+  export LDFLAGS="-L$PREFIX/lib${LDFLAGS+ $LDFLAGS}"
+  export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}"
 
   if [ -x "$PREFIX/profile" ]; then
     . "$PREFIX/profile"
+  fi
+}
+
+process_in_reverse()
+{
+  if [ -z $1 ]; then
+    return 0
+  fi
+
+  local first="$1"
+  shift
+  if ! process_in_reverse $@; then
+    return 1
+  fi
+
+  if ! prepend "$first"; then
+    return 1
+  fi
+}
+
+clean
+
+if [ -z "$1" ]; then
+  echo "Prefix has been unset"
+  return 0
+else
+
+  if ! process_in_reverse $@; then
+    clean
+    return 1
   fi
 
   export CONFIGURE_ARGS="$CONFIGURE_ARGS --prefix=\"$PREFIX\""
 
   echo "Prefix set to $PREFIX"
 fi
+

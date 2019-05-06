93e7533343a1b1c14634c2d73be59d8cde7670f5
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2019-05-06 13:59:39 +0000

    Small fixes to host type detection

--- fzmake/clientscripts/configure_target.sh
+++ fzmake/clientscripts/configure_target.sh
@@ -9,44 +9,43 @@ normalize()
 
 BUILD=`"${SCRIPTS:-.}/config.guess"`
 COMP=`${CC:-cc} $CFLAGS -dumpmachine`
-TARGET="${2:-${1:-$BUILD}}"
+CONFIGURE_HOST="${CONFIGURE_HOST:-${1:-$BUILD}}"
 
-NTARGET=`normalize "$TARGET"`
+NHOST=`normalize "$CONFIGURE_HOST"`
 NBUILD=`normalize "$BUILD"`
 NCOMP=`normalize "$COMP"`
 
 normalize_version()
 {
-  local TARGET="$1"
+  local HOST="$1"
   local BUILD="$2"
 
   if echo "$BUILD" | grep -e '^i[1-9]86-' 2>&1 > /dev/null; then
-    if echo "$TARGET" | grep -e '^i[1-9]86-' 2>&1 > /dev/null; then
+    if echo "$HOST" | grep -e '^i[1-9]86-' 2>&1 > /dev/null; then
       local CPU="${BUILD%%-*}"
-      TARGET="${CPU}-${TARGET#*-}"
+      HOST="${CPU}-${HOST#*-}"
     fi
   fi
 
   if echo "$BUILD" | grep -e '-apple-darwin[0-9.]\+$'  > /dev/null; then
-    if echo "$TARGET" | grep -e '-apple-darwin[0-9.]\+$'  > /dev/null; then
+    if echo "$HOST" | grep -e '-apple-darwin[0-9.]\+$'  > /dev/null; then
       local VERSION="${BUILD##*apple-darwin}"
-      TARGET="${TARGET%apple-darwin*}apple-darwin${VERSION}"
+      HOST="${HOST%apple-darwin*}apple-darwin${VERSION}"
     fi
   fi
 
-  echo "$TARGET"
+  echo "$HOST"
 }
 
-NTARGET=`normalize_version "$NTARGET" "$NBUILD"`
+NHOST=`normalize_version "$NHOST" "$NBUILD"`
 NCOMP=`normalize_version "$NCOMP" "$NBUILD"`
 
-if [ "$NTARGET" = "$NCOMP" ]; then
+if [ "$NHOST" = "$NCOMP" ]; then
   if [ "$NBUILD" != "$NCOMP" ]; then
     echo "Warning: Native compiler ($COMP) does not match build system ($BUILD)" >&2
-    echo "--build=$TARGET --host=$TARGET"
+    echo "--build=$HOST --host=$HOST"
   fi
 else
-  echo "Configuring with --host=$TARGET" >&2
-  echo "--host=$TARGET"
+  echo "Configuring with --host=$CONFIGURE_HOST" >&2
+  echo "--host=$CONFIGURE_HOST"
 fi
-

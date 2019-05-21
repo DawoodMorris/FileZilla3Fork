b7e170e0d6854ec6c91817d091a2fd216e3387be
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2019-05-21 08:59:58 +0000

    Fix distributing packages

--- fzmake/scripts/makedist.sh
+++ fzmake/scripts/makedist.sh
@@ -5,10 +5,10 @@ makedist()
   logprint "Creating source distribution"
   logprint "Configuring FileZilla 3 package"
 
-  mkdir -p "$WORKDIR/fz"
+  mkdir -p "$WORKDIR/dist/fz"
 
-  cd "$WORKDIR/fz"
-  ../source/fz/configure --enable-localesonly >> $LOG 2>&1 || return 1
+  cd "$WORKDIR/dist/fz"
+  ../../source/fz/configure --enable-localesonly >> $LOG 2>&1 || return 1
   make dist >> $LOG 2>&1 || return 1
 
   cp `find *.tar.bz2` "$OUTPUTDIR/FileZilla3-src.tar.bz2" >> $LOG 2>&1 || return 1

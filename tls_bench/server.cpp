08ddd83fe30de34fad59856d64b8dc5d8eccca14
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2019-07-10 07:53:11 +0000

    Parallelize the server

--- tls_bench/server.cpp
+++ tls_bench/server.cpp
@@ -39,71 +39,49 @@ struct connection final
 	size_t to_send_{};
 };
 
-struct server final : public fz::event_handler
+struct accept_event_type;
+typedef fz::simple_event<accept_event_type> accept_event;
+
+struct worker final : public fz::event_handler
 {
-	explicit server(fz::thread_pool & pool, fz::event_loop & loop, unsigned short port);
-	virtual ~server();
+	explicit worker(fz::event_loop & loop, fz::listen_socket& ls);
+	virtual ~worker();
 
 	virtual void operator()(fz::event_base const& ev) override;
 	void on_socket_event(fz::socket_event_source*, fz::socket_event_flag t, int error);
+	void on_accept();
 
 	void on_write(std::unordered_map<fz::socket_event_source *, connection>::iterator it);
 
 	fz::event_loop & loop_;
 
-	std::unique_ptr<fz::listen_socket> ls_;
+	fz::listen_socket & ls_;
 
 	std::unordered_map<fz::socket_event_source *, connection> connections_;
 };
 
-server::server(fz::thread_pool & pool, fz::event_loop & loop, unsigned short port)
+worker::worker(fz::event_loop & loop, fz::listen_socket & ls)
 	: fz::event_handler(loop)
 	, loop_(loop)
+	, ls_(ls)
 {
-	ls_ = std::make_unique<fz::listen_socket>(pool, this);
-	if (ls_->listen(fz::address_type::unknown, static_cast<int>(port)) != 0) {
-		std::cerr << "Listen failed\n";
-		exit(1);
-	}
-	std::cerr << "Now listening on port " << port << "\n";
 }
 
-server::~server()
+worker::~worker()
 {
 	remove_handler();
-	ls_.reset();
+	connections_.clear();
 }
 
-void server::operator()(fz::event_base const& ev)
+void worker::operator()(fz::event_base const& ev)
 {
-	fz::dispatch<fz::socket_event>(ev, this,
-		&server::on_socket_event);
+	fz::dispatch<fz::socket_event, accept_event>(ev, this,
+		&worker::on_socket_event,
+		&worker::on_accept);
 }
 
-void server::on_socket_event(fz::socket_event_source* s, fz::socket_event_flag t, int error)
+void worker::on_socket_event(fz::socket_event_source* s, fz::socket_event_flag t, int error)
 {
-	if (s == ls_.get()) {
-		int err{};
-
-		connection conn;
-		conn.s_ = ls_->accept(err);
-		if (!conn.s_) {
-			std::cout << "c" << std::flush;
-			return;
-		}
-		conn.s_->set_flags(conn.s_->flags() | fz::socket::flag_nodelay);
-
-		conn.tls_ = std::make_unique<fz::tls_layer>(loop_, this, *conn.s_, nullptr, log);
-		conn.tls_->set_certificate(get_key_and_cert().first, get_key_and_cert().second, fz::native_string());
-		if (!conn.tls_->server_handshake()) {
-			std::cout << "h" << std::flush;
-			return;
-		}
-		conn.layer_ = conn.tls_.get();
-		connections_[conn.s_.get()] = std::move(conn);
-		return;
-	}
-
 	auto it = connections_.find(s->root());
 	auto & conn = it->second;
 
@@ -136,7 +114,7 @@ void server::on_socket_event(fz::socket_event_source* s, fz::socket_event_flag t
 					error = conn.layer_->shutdown();
 					if (!error) {
 						if (conn.eof_ == 2) {
-							std::cout << "." << std::flush;
+//							std::cout << "." << std::flush;
 							connections_.erase(it);
 							return;
 						}
@@ -186,7 +164,29 @@ void server::on_socket_event(fz::socket_event_source* s, fz::socket_event_flag t
 	}
 }
 
-void server::on_write(std::unordered_map<fz::socket_event_source *, connection>::iterator it)
+void worker::on_accept()
+{
+	int err{};
+
+	connection conn;
+	conn.s_ = ls_.accept(err);
+	if (!conn.s_) {
+		std::cout << "c" << std::flush;
+		return;
+	}
+	conn.s_->set_flags(conn.s_->flags() | fz::socket::flag_nodelay);
+
+	conn.tls_ = std::make_unique<fz::tls_layer>(loop_, this, *conn.s_, nullptr, log);
+	conn.tls_->set_certificate(get_key_and_cert().first, get_key_and_cert().second, fz::native_string());
+	if (!conn.tls_->server_handshake()) {
+		std::cout << "h" << std::flush;
+		return;
+	}
+	conn.layer_ = conn.tls_.get();
+	connections_[conn.s_.get()] = std::move(conn);
+}
+
+void worker::on_write(std::unordered_map<fz::socket_event_source *, connection>::iterator it)
 {
 	auto & conn = it->second;
 
@@ -214,7 +214,7 @@ void server::on_write(std::unordered_map<fz::socket_event_source *, connection>:
 		int error = conn.layer_->shutdown();
 		if (!error) {
 			if (conn.eof_ == 2) {
-				std::cout << "." << std::flush;
+//				std::cout << "." << std::flush;
 				connections_.erase(it);
 			}
 		}
@@ -228,22 +228,91 @@ void server::on_write(std::unordered_map<fz::socket_event_source *, connection>:
 	}
 }
 
+
+struct server final : public fz::event_handler
+{
+	explicit server(fz::thread_pool & pool, fz::event_loop & loop, unsigned short port, size_t workers);
+	virtual ~server();
+
+	virtual void operator()(fz::event_base const& ev) override;
+	void on_socket_event(fz::socket_event_source*, fz::socket_event_flag t, int error);
+
+	fz::event_loop & loop_;
+
+	std::unique_ptr<fz::listen_socket> ls_;
+
+	std::vector<std::pair<std::unique_ptr<fz::event_loop>, std::unique_ptr<worker>>> workers_;
+	size_t worker_{};
+};
+
+
+server::server(fz::thread_pool & pool, fz::event_loop & loop, unsigned short port, size_t workers)
+	: fz::event_handler(loop)
+	, loop_(loop)
+{
+	ls_ = std::make_unique<fz::listen_socket>(pool, this);
+	if (ls_->listen(fz::address_type::unknown, static_cast<int>(port)) != 0) {
+		std::cerr << "Listen failed\n";
+		exit(1);
+	}
+	std::cerr << "Now listening on port " << port << "\n";
+
+	workers_.reserve(workers);
+	while (workers--) {
+		auto wl = std::make_unique<fz::event_loop>(pool);
+		workers_.push_back(std::make_pair(std::move(wl), std::make_unique<worker>(*wl, *ls_)));
+	}
+}
+
+server::~server()
+{
+	remove_handler();
+	ls_.reset();
+}
+
+void server::operator()(fz::event_base const& ev)
+{
+	fz::dispatch<fz::socket_event>(ev, this,
+		&server::on_socket_event);
+}
+
+void server::on_socket_event(fz::socket_event_source* s, fz::socket_event_flag, int error)
+{
+	if (error) {
+		return;
+	}
+	if (s != ls_.get()) {
+		return;
+	}
+
+	auto & w = workers_[worker_].second;
+	w->send_event<accept_event>();
+	if (++worker_ == workers_.size()) {
+		worker_ = 0;
+	}
+}
+
 int main(int argc, char const ** const argv)
 {
-	if (argc < 2) {
-		std::cerr << "Need to pass port" << std::endl;
+	if (argc < 3) {
+		std::cerr << "Need to pass port and workers\n";
 		exit(1);
 	}
 
 	send_buffer = fz::random_bytes(1024 * 1024);
 
-	int port = fz::to_integral<unsigned short>(std::string_view(argv[1]));
+	unsigned short const port = fz::to_integral<unsigned short>(std::string_view(argv[1]));
+	size_t const workers = fz::to_integral<size_t>(std::string_view(argv[2]));
+	if (!workers) {
+		std::cerr << "No workers.\n";
+		exit(1);
+	}
 
 	fz::thread_pool pool;
 
 	fz::event_loop loop(fz::event_loop::threadless);
 
-	server s(pool, loop, port);
+	server s(pool, loop, port, workers);
 
 	loop.run();
 

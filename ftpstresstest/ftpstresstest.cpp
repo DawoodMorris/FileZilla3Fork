2517a9961a459871fd259a2b9f78e5a17ec8c457
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2021-02-04 16:56:38 +0000

    Enhance stresstest

--- ftpstresstest/ftpstresstest.cpp
+++ ftpstresstest/ftpstresstest.cpp
@@ -13,6 +13,7 @@
 #include <map>
 #include <iostream>
 #include <string>
+#include <string.h>
 
 namespace {
 // Helper function to extract a directory from argv[0] so that the
@@ -60,7 +61,6 @@ struct logger : public fz::logger_interface
 
 }
 
-
 class client final : public fz::event_handler
 {
 public:
@@ -87,6 +87,10 @@ public:
 
 		add_timer(fz::duration::from_seconds(2), false);
 		lastread_ = fz::datetime::now();
+
+		if (fz::random_number(0, 1) == 1) {
+			// Ignore login, go straight to noop storm
+		}
 	}
 
 	~client()
@@ -102,7 +106,7 @@ public:
 	void on_timer(fz::timer_id const&)
 	{
 		if ((fz::datetime::now() - lastread_) >= fz::duration::from_seconds(2)) {
-			std::cerr << "Read timeout\n";
+			std::cerr << "Read timeout in state " << state_ << ", total read: " << total_read_ << ", total sent: " << total_sent_ << "\n";
 			exit(1);
 		}
 	}
@@ -110,12 +114,18 @@ public:
 	void on_cert(fz::tls_layer* l, fz::tls_session_info const&)
 	{
 		l->set_verification_result(true);
+
+		if (state_ == handshake) {
+			++state_;
+			can_send_ = false;
+			send_next();
+		}
 	}
 
 	void on_socket_event(fz::socket_event_source * s, fz::socket_event_flag flag, int error)
 	{
 		if (error) {
-			std::cerr << "Socket error on event " << (int)flag << "\n";
+			std::cerr << "Socket error " << error << " on event " << (int)flag << " in state " << state_ << "\n";
 			exit(1);
 		}
 		if (flag == fz::socket_event_flag::read) {
@@ -131,32 +141,160 @@ public:
 
 
 			if (r > 0) {
+				total_read_ += r;
 				lastread_ = fz::datetime::now();
+				inbuffer_.add(r);
+				parse_input();
 				send_event<fz::socket_event>(s, flag, 0);
 			}
 		}
-		else if (flag == fz::socket_event_flag::write) {
-			while (outbuffer_.size() < 4096) {
-				outbuffer_.append("NOOP\r\n");
-			}
-
-			int w = si_->write(outbuffer_.get(), outbuffer_.size(), error);
-			if (!w) {
-				std::cerr << "EOF\n";
-				exit(1);
-			}
-			if (w < 0 && error != EAGAIN) {
-				std::cerr << "Error writing: " << error << "\n";
-				exit(1);
-			}
-
-			if (w > 0) {
-				outbuffer_.consume(w);
-				send_event<fz::socket_event>(s, flag, 0);
-			}
+		else if (flag == fz::socket_event_flag::write || flag == fz::socket_event_flag::connection) {
+			prepare_send();
+			do_send();
 		}
 	}
 
+	void do_send()
+	{
+		can_send_ = false;
+
+		if (outbuffer_.empty()) {
+			can_send_ = true;
+			return;
+		}
+
+		int error;
+		int w = si_->write(outbuffer_.get(), outbuffer_.size(), error);
+
+		if (!w) {
+			std::cerr << "EOF\n";
+			exit(1);
+		}
+		if (w < 0 && error != EAGAIN) {
+			std::cerr << "Error writing: " << error << "\n";
+			exit(1);
+		}
+
+		if (w > 0) {
+			total_sent_ += w;
+			outbuffer_.consume(w);
+			send_event<fz::socket_event>(si_, fz::socket_event_flag::write, 0);
+		}
+	}
+
+	void parse_input()
+	{
+		if (state_ == noop) {
+			inbuffer_.clear();
+			return;
+		}
+
+		while (true) {
+			size_t i;
+			for (i = 0; i < inbuffer_.size(); ++i) {
+				if (inbuffer_[i] == '\n') {
+					break;
+				}
+			}
+			if (i == inbuffer_.size()) {
+				return;
+			}
+
+			if (!multiline_.empty()) {
+				if (i < 5 || memcmp(inbuffer_.get(), multiline_.c_str(), 4)) {
+					inbuffer_.consume(i + 1);
+					continue;
+				}
+				multiline_.clear();
+			}
+
+			if (i < 5 || inbuffer_[0] < '1' || inbuffer_[0] > '3') {
+				std::cerr << "Failed command in state " << state_ << ": " << std::string_view(reinterpret_cast<char const*>(inbuffer_.get()), i) << "\n";
+				exit(1);
+			}
+
+
+			if (inbuffer_[3] == '-') {
+				multiline_ = std::string(reinterpret_cast<char const*>(inbuffer_.get()), 3);
+				multiline_ += ' ';
+				inbuffer_.consume(i + 1);
+				continue;
+			}
+
+			inbuffer_.consume(i + 1);
+
+			++state_;
+
+			send_next();
+		}
+	}
+
+	void prepare_send()
+	{
+		if (state_ != noop) {
+			return;
+		}
+		while (outbuffer_.size() < 4096) {
+			outbuffer_.append("NOOP\r\n");
+		}
+	}
+
+	void send_next()
+	{
+		switch (state_) {
+		case auth:
+			if (creds_.implicit_tls) {
+				state_ += 2;
+				send_next();
+			}
+			else {
+				outbuffer_.append("AUTH TLS\r\n");
+			}
+			break;
+		case handshake:
+			if (creds_.implicit_tls) {
+				++state_;
+				send_next();
+			}
+			else {
+				tls_ = std::make_unique<fz::tls_layer>(event_loop_, this, *s_, nullptr, logger_);
+				si_ = tls_.get();
+
+				if (!tls_->client_handshake(this)) {
+					std::cerr << "Handshake failed\n";
+					exit(1);
+				}
+			}
+			break;
+		case user:
+			outbuffer_.append("USER " + creds_.user + "\r\n");
+			break;
+		case pass:
+			outbuffer_.append("PASS " + creds_.pass + "\r\n");
+			break;
+		case noop:
+			while (outbuffer_.size() < 4096) {
+				outbuffer_.append("NOOP\r\n");
+			}
+			break;
+		}
+
+		if (can_send_) {
+			do_send();
+		}
+	}
+
+	enum state {
+		welcome,
+		auth,
+		handshake,
+		user,
+		pass,
+		noop
+	};
+	int state_{};
+
+	std::string multiline_;
 	fz::buffer inbuffer_;
 	fz::buffer outbuffer_;
 	credentials creds_;
@@ -168,6 +306,11 @@ public:
 	std::unique_ptr<fz::socket> s_;
 	std::unique_ptr<fz::tls_layer> tls_;
 	fz::socket_interface* si_{};
+
+	size_t total_read_{};
+	size_t total_sent_{};
+
+	bool can_send_{};
 };
 
 class runner final : public fz::event_handler

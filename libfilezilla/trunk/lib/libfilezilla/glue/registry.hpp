1b55c1583356f9b27e45187ef0e8f96acb8190ca
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2022-02-28 15:50:43 +0000

    Registry handling

---
+++ libfilezilla/trunk/lib/libfilezilla/glue/registry.hpp
@@ -0,0 +1,56 @@
+#ifndef LIBFILEZILLA_GLUE_REGISTRY_HEADER
+#define LIBFILEZILLA_GLUE__REGISTRY_HEADER
+
+#include "../libfilezilla.h"
+
+#ifdef FZ_WINDOWS
+
+#include <optional>
+#include <string>
+
+#include "windows.h"
+
+namespace fz {
+class FZ_PUBLIC_SYMBOL regkey final
+{
+public:
+	regkey() = default;
+	~regkey();
+
+	enum regview {
+		regview_native,
+		regview_32,
+		regview_64
+	};
+
+	explicit regkey(HKEY const root, std::wstring const& subkey, bool readonly, regview v = regview_native);
+
+	regkey(regkey const&) = delete;
+	regkey& operator=(regkey const&) = delete;
+
+	void close();
+
+	bool open(HKEY const root, std::wstring const& subkey, bool readonly, regview v = regview_native);
+
+	bool has_value(std::wstring const& name) const;
+
+	std::wstring value(std::wstring const& name) const;
+	uint64_t int_value(std::wstring const& name) const;
+
+	bool set_value(std::wstring const& name, std::wstring const& value);
+	bool set_value(std::wstring const& name, uint64_t value);
+
+	explicit operator bool() const {
+		return key_.has_value();
+	}
+
+private:
+	mutable std::optional<HKEY> key_;
+};
+}
+
+#else
+#error This file is for Windows only
+#endif
+
+#endif
